55422cdb332b0c1e89562c40872a2493b8ae1c2e
Supun200110 <supunnishantha45@gmail.com>
2025-02-10 15:30:36 +0530

    I created Product View Product Page

---
+++ app/src/main/java/com/example/mobileapp/View_Product.java
@@ -0,0 +1,93 @@
+package com.example.mobileapp;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class View_Product extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_view_product);
+
+        getSupportActionBar().hide();
+
+
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(View_Product.this,Payment.class);
+                startActivity(intent);
+            }
+        });
+
+        View mainLayout = findViewById(R.id.main);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+
+        mainLayout.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            Rect r = new Rect();
+            mainLayout.getWindowVisibleDisplayFrame(r);
+            int screenHeight = mainLayout.getRootView().getHeight();
+            int keypadHeight = screenHeight - r.bottom;
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // Keyboard is open
+                bottomNavigationView.setVisibility(View.GONE);
+            } else {
+                // Keyboard is closed
+                bottomNavigationView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // Deselect All Items
+        bottomNavigationView.getMenu().setGroupCheckable(0, true, false);
+        for (int i = 0; i < bottomNavigationView.getMenu().size(); i++) {
+            bottomNavigationView.getMenu().getItem(i).setChecked(false);
+        }
+        bottomNavigationView.getMenu().setGroupCheckable(0, true, true);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.bottom_home) {
+                startActivity(new Intent(getApplicationContext(), Home.class));
+
+                return true;
+            } else if (id == R.id.bottom_search) {
+                startActivity(new Intent(getApplicationContext(), Search.class));
+
+                return true;
+            } else if (id == R.id.bottom_Cart) {
+                startActivity(new Intent(getApplicationContext(), View_Cart.class));
+
+                return true;
+            }
+
+            return false;
+        });
+    }
+}
