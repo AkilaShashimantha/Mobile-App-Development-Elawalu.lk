7e12f69c662d473843edb672eb722b0502bd0ded
Ayesh Madhuranga <ayeshmaduranga2001@gmail.com>
2025-02-10 15:02:34 +0530

    Vegetable Sellers Page

---
+++ app/src/main/java/com/example/mobileapp/Vegetable_Sellers.java
@@ -0,0 +1,75 @@
+package com.example.mobileapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Vegetable_Sellers extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vegetable_sellers);
+
+        getSupportActionBar().hide();
+
+        View mainLayout = findViewById(R.id.main);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+
+        mainLayout.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            Rect r = new Rect();
+            mainLayout.getWindowVisibleDisplayFrame(r);
+            int screenHeight = mainLayout.getRootView().getHeight();
+            int keypadHeight = screenHeight - r.bottom;
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // Keyboard is open
+                bottomNavigationView.setVisibility(View.GONE);
+            } else {
+                // Keyboard is closed
+                bottomNavigationView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_search);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.bottom_home) {
+                startActivity(new Intent(getApplicationContext(), Home.class));
+                finish();
+
+            } else if (id == R.id.bottom_search) {
+                startActivity(new Intent(getApplicationContext(), Search.class));
+            } else if (id == R.id.bottom_Cart) {
+                startActivity(new Intent(getApplicationContext(), View_Cart.class));
+
+                return true;
+            }
+
+            return false;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
